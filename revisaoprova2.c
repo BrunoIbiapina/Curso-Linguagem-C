8dec15e9d04aa9a19f983832b6ec792fc7768d48
Bruno Ibiapina <brunoibiapina@me.com>
2024-04-20 13:44:09 -0300

    Exercícios Faculdade

---
+++ revisaoprova2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int main(){
+int valor1,valor2,valor3;
+printf("Digite um valor: \n");
+scanf("%d %d %d", &valor1,&valor2,&valor3);
+int maiorvalor = valor1;
+if(valor1>valor2){
+    maiorvalor = valor1;
+}else{
+    maiorvalor=valor2;
+}
+if(valor2>valor3){
+    maiorvalor = valor2;
+}else{
+    maiorvalor=valor3;
+}
+
+printf("O maior valor dos 3 números é: %d\n", maiorvalor);
+}
---
+++ revisãoprova.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float ProdutoA, ProdutoB, ProdutoC, SomaAB, SomaBC;
+
+int main () {
+    printf("Informe valor do seu primeiro produto A \n");
+    scanf("%f", &ProdutoA);
+    printf("Informe valor do seu segundo produto B \n");
+    scanf("%f", &ProdutoB);
+    printf("Informe valor do seu terceiro produto C \n");
+    scanf("%f", &ProdutoC);
+    SomaAB = ProdutoA + ProdutoB;
+    if (SomaAB > ProdutoC) {
+        printf("O valor da soma de A + B é: %.2f\n", SomaAB);
+    }else{
+        printf("O valor do Produto C é: %.2f\n", ProdutoC);
+    }
+    SomaBC = ProdutoB + ProdutoC;
+    if (SomaBC > ProdutoA){
+        printf("O valor da Soma de B + C é: %2.f\n", SomaBC);
+    }else{
+        printf("O valor do Produto A é: %2.f\n ", ProdutoA);
+    }
+    
+}
+
