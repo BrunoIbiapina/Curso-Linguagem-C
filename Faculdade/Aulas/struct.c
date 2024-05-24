31b2863e6ac378cfe5579a7285cfc59219d93637
Bruno Ibiapina <brunoibiapina@me.com>
2024-05-23 21:44:50 -0300

    ajuste

--- Faculdade/Aulas/struct.c
+++ Faculdade/Aulas/struct.c
@@ -11,31 +11,29 @@ struct cadastroProduto{
 };
 int main () {
 int i;
-    struct cadastroProduto produto [5];
-    for (i = 0; i < 3 ; i++) {
+    struct cadastroProduto produto [2];/*Estrutura de repetição para Leitura*/
+    for (i = 0; i < 2 ; i++) {
         printf("Digite o codigo do produto \n");
         scanf("%d",  &produto[i].codigo);
 
         printf("Digite a descrição do produto \n");
-        scanf("%s", produto[i].nome);
+        scanf("%s", &produto[i].nome);
         
         printf("Digite a quantidade do produto \n");
-        scanf("%f", &produto[i].quantidade);
+        scanf("%d", &produto[i].quantidade);
         
         printf("Digite o valor unitario do produto \n");
         scanf("%f", &produto[i].valorUnitario);
    
-      }
+    }
     
-    for (i = 0; i < 5; i++) {
-        printf("Produto %d\n", i + 1);
-        printf("Codigo: %d\n", produto[i].codigo);
-        printf("Nome: %s\n", produto[i].nome);
-        printf("Quantidade: %.2f\n", produto[i].quantidade);
-        printf("Valor Unitario: %.2f\n", produto[i].valorUnitario);
+    for (i = 0; i < 2; i++) {
+        printf("Codigo...............: %d\n", produto[i].codigo);
+        printf("Descrição............: %s\n", produto[i].nome);
+        printf("Quantidade...........: %d\n", produto[i].quantidade);
+        printf("Valor Unitario.......: %.2f\n", produto[i].valorUnitario);
     }
 
-    return 0;
 }
  
 
